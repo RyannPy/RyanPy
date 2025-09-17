b269f17cdd341e3470256ff82ecf4ae5e94b1980
RyanPy <ryanbayu1233@gmail.com>
2025-09-17 13:13:09 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// NAVBAR
 const menuButton = document.getElementById("menu-button");
 const sidebar = document.querySelector(".sidebar");
+
 const container = document.querySelector(".container"); // konten utama
 
 menuButton.addEventListener("click", () => {
@@ -7,3 +9,16 @@ menuButton.addEventListener("click", () => {
   sidebar.classList.toggle("active");       // sidebar slide
   container.classList.toggle("blur");         // blur konten
 });
+
+// ANIMASI SCROLL
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate');
+    }
+  });
+}, { threshold: 0.3 });
+
+document.querySelectorAll('.slide-left, .slide-right, .slide-top, .slide-bottom, .progress')
+  .forEach(el => observer.observe(el));
+
